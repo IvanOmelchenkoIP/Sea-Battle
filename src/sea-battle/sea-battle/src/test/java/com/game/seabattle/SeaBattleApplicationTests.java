aa6b2e2d7847c90310e84d778220e9b445fa1912
IvanOmelchenkoIP <io25112002@gmail.com>
2023-12-21 23:09:16 +0200

    Add project files
    Add project files for future coding

---
+++ src/sea-battle/sea-battle/src/test/java/com/game/seabattle/SeaBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.seabattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
